26b83e8e90acd714347456006066347b873d2582
Veronica Harris <s2vlharr@hotmail.com>
2023-01-31 16:14:16 -0500

    new changes

---
+++ goldpeach.js
@@ -0,0 +1,16 @@
+
+function createAccount() {
+    document.getElementById("account").innerHTML = "Your account has been created and a new library card will be mailed the address provided. A PIN is needed when logging into your account. Your PIN is the 4-digit month and day of your date of birth. Format MMDD.";
+}
+
+
+
+// function createAccount() {
+//     var x = document.getElementById("form");
+//     if (x.style.display === "none") {
+//         x.style.display = "block";
+//     } else {
+//         x.style.display = "none";
+//     }
+// }
+
